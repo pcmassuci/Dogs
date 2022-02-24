eed964b7e01f6b911e963670b7e9b173772dc3b0
Paulo César Morandi Massuci <paulo.massuci.ext@sodexo.com>
2022-02-24 19:04:07 -0300

    Create Project

---
+++ Dogs/Dogs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dogs
+//
+//  Created by Paulo César Morandi Massuci on 24/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
