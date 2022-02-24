8cb45fb2cf6f3c963fdd766dcab96e8c16647b75
Paulo César Morandi Massuci <paulo.massuci.ext@sodexo.com>
2022-02-24 19:17:45 -0300

    Add PODS and Swift Lint

---
+++ Dogs/Pods/Target Support Files/Pods-Dogs-DogsUITests/Pods-Dogs-DogsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Dogs_DogsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Dogs_DogsUITests
+@end
---
+++ Dogs/Pods/Target Support Files/Pods-DogsTests/Pods-DogsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DogsTests : NSObject
+@end
+@implementation PodsDummy_Pods_DogsTests
+@end
